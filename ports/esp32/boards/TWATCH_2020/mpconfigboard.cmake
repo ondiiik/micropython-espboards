a5590c6cb4b2b1d687bf4779cdfd8798d9e28ea1
OSi (Ondrej Sienczak) <ondrej.sienczak@seznam.cz>
2021-03-23 22:43:00 +0100

    Added support for TWATCH_2020 board

---
+++ ports/esp32/boards/TWATCH_2020/mpconfigboard.cmake
@@ -0,0 +1,8 @@
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.base16m
+    boards/sdkconfig.ble
+    boards/sdkconfig.spiram
+)
+
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
