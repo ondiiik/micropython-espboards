5d418a447db67d73aa1bb6565d367667e4d74486
OSi (Ondrej Sienczak) <ondrej.sienczak@seznam.cz>
2021-04-02 22:55:58 +0200

    Added himem set

--- ports/esp32/boards/TWATCH_2020/drivers/himem.c
+++ ports/esp32/boards/TWATCH_2020/drivers/himem.c
@@ -50,7 +50,9 @@
 
 STATIC esp_himem_handle_t      _himem;
 STATIC esp_himem_rangehandle_t _range;
-STATIC size_t                  _size;
+STATIC bool                    _size_locked;
+STATIC size_t                  _size_raw;
+STATIC size_t                  _size_fs;
 
 
 
@@ -65,30 +67,37 @@ STATIC uintptr_t _align(uintptr_t aMem)
 enum _Direction
 {
     HIMEM_2_RAM,
-    RAM_2_HIMEM
+    RAM_2_HIMEM,
+    HIMEM_SET
 };
 
 
-STATIC void _dir_copy(void*           aRam,
-                      void*           aHimem,
-                      size_t          aSize,
-                      enum _Direction aDir)
+STATIC void _dir_copy(void*    aRam,
+                      void*    aHimem,
+                      size_t   aSize,
+                      unsigned aDir)
 {
-    if (HIMEM_2_RAM == aDir)
+    enum _Direction dir = (enum _Direction)(aDir & 255);
+    
+    if (HIMEM_2_RAM == dir)
     {
         memcpy(aRam, aHimem, aSize);
     }
-    else
+    else if (RAM_2_HIMEM == dir)
     {
         memcpy(aHimem, aRam, aSize);
+    }
+    else
+    {
+        memset(aHimem, aDir >> 8, aSize);
     }
 }
 
 
-STATIC void _himem_copy(void*           aRam,
-                        uintptr_t       aHimem,
-                        size_t          aSize,
-                        enum _Direction aDir)
+STATIC void _himem_op(void*           aRam,
+                      uintptr_t       aHimem,
+                      size_t          aSize,
+                      enum _Direction aDir)
 {
     /*
      * Copy unaligned head
@@ -177,9 +186,9 @@ STATIC void _obj2buff(mp_buffer_info_t* aBuff,
 
 STATIC mp_obj_t himem_init(void)
 {
-    _size = esp_himem_get_free_size();
+    _size_fs = esp_himem_get_free_size();
     
-    ESPY__DO_OR_DIE(esp_himem_alloc(_size, &_himem),                     MemoryError);
+    ESPY__DO_OR_DIE(esp_himem_alloc(_size_fs, &_himem),                     MemoryError);
     ESPY__DO_OR_DIE(esp_himem_alloc_map_range(ESP_HIMEM_BLKSZ, &_range), MemoryError);
     
     return mp_const_none;
@@ -189,11 +198,70 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_0(himem_init_obj, himem_init);
 
 
 
-STATIC mp_obj_t himem_size(void)
+STATIC mp_obj_t himem_size_fs(size_t          aArgsCnt,
+                              const mp_obj_t* aArgs)
 {
-    return MP_OBJ_NEW_SMALL_INT(_size);
+    if (aArgsCnt > 0)
+    {
+        if (_size_locked)
+        {
+            mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("Size of area locked"));
+        }
+        
+        if (!mp_obj_is_small_int(aArgs[0]))
+        {
+            mp_raise_msg(&mp_type_TypeError, MP_ERROR_TEXT("Size of area shall be an integer"));
+        }
+        
+        size_t size     = _size_raw + _size_fs;
+        size_t new_size = MP_OBJ_SMALL_INT_VALUE(aArgs[0]);
+        
+        if (new_size > size)
+        {
+            mp_raise_msg(&mp_type_ValueError, MP_ERROR_TEXT("Requested size of area to large"));
+        }
+        
+        _size_fs  = new_size;
+        _size_raw = size - new_size;
+    }
+    
+    return MP_OBJ_NEW_SMALL_INT(_size_fs);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(himem_size_obj, himem_size);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR(himem_size_fs_obj, 0, himem_size_fs);
+
+
+
+
+STATIC mp_obj_t himem_size_raw(size_t          aArgsCnt,
+                               const mp_obj_t* aArgs)
+{
+    if (aArgsCnt > 0)
+    {
+        if (_size_locked)
+        {
+            mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("Size of area locked"));
+        }
+        
+        if (!mp_obj_is_small_int(aArgs[0]))
+        {
+            mp_raise_msg(&mp_type_TypeError, MP_ERROR_TEXT("Size of area shall be an integer"));
+        }
+        
+        size_t size     = _size_raw + _size_fs;
+        size_t new_size = MP_OBJ_SMALL_INT_VALUE(aArgs[0]);
+        
+        if (new_size > size)
+        {
+            mp_raise_msg(&mp_type_ValueError, MP_ERROR_TEXT("Requested size of area to large"));
+        }
+        
+        _size_fs  = size - new_size;
+        _size_raw = new_size;
+    }
+    
+    return MP_OBJ_NEW_SMALL_INT(_size_raw);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR(himem_size_raw_obj, 0, himem_size_raw);
 
 
 
@@ -210,13 +278,20 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_0(himem_page_obj, himem_page);
 STATIC mp_obj_t himem_read(mp_obj_t aAddress,
                            mp_obj_t aSize)
 {
+    _size_locked = true;
+    
     uintptr_t      addr = mp_obj_get_int(aAddress);
     size_t         size = mp_obj_get_int(aSize);
     vstr_t         bytes;
     
+    if (addr + size > _size_raw)
+    {
+        mp_raise_msg(&mp_type_MemoryError, MP_ERROR_TEXT("Area out of range"));
+    }
+    
     vstr_init_len(&bytes, size);
     
-    _himem_copy(bytes.buf, addr, size, HIMEM_2_RAM);
+    _himem_op(bytes.buf, addr, size, HIMEM_2_RAM);
     
     return mp_obj_new_str_from_vstr(&mp_type_bytes, &bytes);
 }
@@ -228,11 +303,19 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_2(himem_read_obj, himem_read);
 STATIC mp_obj_t himem_write(mp_obj_t aAddress,
                             mp_obj_t aBuffer)
 {
+    _size_locked = true;
+    
     uintptr_t        addr  = mp_obj_get_int(aAddress);
     mp_buffer_info_t buff;
     
     _obj2buff(&buff, aBuffer, MP_BUFFER_READ);
-    _himem_copy(buff.buf, addr, buff.len, RAM_2_HIMEM);
+    
+    if (addr + buff.len > _size_raw)
+    {
+        mp_raise_msg(&mp_type_MemoryError, MP_ERROR_TEXT("Area out of range"));
+    }
+    
+    _himem_op(buff.buf, addr, buff.len, RAM_2_HIMEM);
     
     return mp_const_none;
 }
@@ -241,14 +324,40 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_2(himem_write_obj, himem_write);
 
 
 
+STATIC mp_obj_t himem_set(mp_obj_t aAddress,
+                          mp_obj_t aValue,
+                          mp_obj_t aSize)
+{
+    _size_locked = true;
+    
+    uintptr_t addr  = mp_obj_get_int(aAddress);
+    unsigned  value = mp_obj_get_int(aValue);
+    size_t    size  = mp_obj_get_int(aSize);
+    
+    if (addr + size > _size_raw)
+    {
+        mp_raise_msg(&mp_type_MemoryError, MP_ERROR_TEXT("Area out of range"));
+    }
+    
+    _himem_op(NULL, addr, size, HIMEM_SET + (value << 8));
+    
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(himem_set_obj, himem_set);
+
+
+
+
 STATIC const mp_map_elem_t globals_dict_table[] =
 {
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_himem)   },
-    { MP_ROM_QSTR(MP_QSTR___init__), MP_ROM_PTR(&himem_init_obj)  },
-    { MP_ROM_QSTR(MP_QSTR_size),     MP_ROM_PTR(&himem_size_obj)  },
-    { MP_ROM_QSTR(MP_QSTR_page),     MP_ROM_PTR(&himem_page_obj)  },
-    { MP_ROM_QSTR(MP_QSTR_read),     MP_ROM_PTR(&himem_read_obj)  },
-    { MP_ROM_QSTR(MP_QSTR_write),    MP_ROM_PTR(&himem_write_obj) }
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_himem)      },
+    { MP_ROM_QSTR(MP_QSTR___init__), MP_ROM_PTR(&himem_init_obj)     },
+    { MP_ROM_QSTR(MP_QSTR_size_fs),  MP_ROM_PTR(&himem_size_fs_obj)  },
+    { MP_ROM_QSTR(MP_QSTR_size_raw), MP_ROM_PTR(&himem_size_raw_obj) },
+    { MP_ROM_QSTR(MP_QSTR_page),     MP_ROM_PTR(&himem_page_obj)     },
+    { MP_ROM_QSTR(MP_QSTR_read),     MP_ROM_PTR(&himem_read_obj)     },
+    { MP_ROM_QSTR(MP_QSTR_write),    MP_ROM_PTR(&himem_write_obj)    },
+    { MP_ROM_QSTR(MP_QSTR_set),      MP_ROM_PTR(&himem_set_obj)      }
 };
 STATIC MP_DEFINE_CONST_DICT(globals_dict, globals_dict_table);
 
